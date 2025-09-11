704398c5a7a3c7927e6214ed50f2a83f628d26af
Vitor Lopes <vcoder00@gmail.com>
2025-09-10 22:21:05 -0300

    chore: clean up redundant logic and improve file organization

--- src/app.ts
+++ src/app.ts
@@ -16,24 +16,38 @@ class Quote {
     }
 }
 
-let ListaDeFrases: Array<Quote> = [];
+let listaDeFrases: Array<Quote> = [];
+
+let currentQuote: Quote;
 
 async function fetchNewPhrase(): Promise<void> {
     fetch("https://echoes.soferity.com/api/quotes/random?lang=en")
-    .then((response) => response.json()) // transforma o json recebido em um
-    .then((data) => {
-        console.log(data);
+        .then((response) => response.json()) // transforma o json recebido em um objeto
+        .then((data) => {
+            console.log(data);
 
-    // A API  retorna um array, então pegamos o primeiro item com a propriedade correta
-    const quoteText = data.quote;
-    const authorName = data.author.replace(/'/g, '');
+            // A API  retorna um array, então pegamos o primeiro item com a propriedade correta
+            const quote = new Quote(data.quote, data.author)
 
-    const ph1 = new Quote(quoteText, authorName);
-    phrase.textContent = ph1.text;
-    author.textContent = ph1.authorship;
-    }
-    )
-    .catch(console.error);
+            exibicaoPrincipal(quote);
+
+            currentQuote = quote;
+
+            return quote;
+        }
+        )
+        .catch(console.error);
 }
+
+function exibicaoPrincipal(quote: Quote): void {
+    phrase.textContent = quote.text;
+    author.textContent = quote.authorship;
+}
+
+function saveNewPhrase(): void {
+    listaDeFrases.push(currentQuote);
+    console.log(listaDeFrases);
+}
+
 btnNewPhrase.addEventListener('click', fetchNewPhrase);
-fetchNewPhrase();
+btnSave.addEventListener('click', saveNewPhrase)
